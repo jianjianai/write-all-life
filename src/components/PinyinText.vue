84d61d052d8036809f8f41fbc047506ec8091cb8
jjaw <jjaw@jjaw.cn>
2023-11-16 20:51:07 +0800

    错误显示红色

--- src/components/PinyinText.vue
+++ src/components/PinyinText.vue
@@ -2,25 +2,21 @@
 import { pinyin } from 'pinyin-pro';
 import { ref, watch, type Ref, toRefs } from 'vue';
 
-let props = defineProps<{ text: string, word: string, shwoWord: boolean,inputWord:string }>();
+let props = defineProps<{ text: string, word: string, shwoWord: boolean, inputWord: string }>();
 let { text, word, shwoWord, inputWord } = toRefs(props);
 
-let shwoArray: Ref<{ t: string, up?: string, is_?:boolean}[]> = ref([]);
+let shwoArray: Ref<{ ty: string, t?: string, up?: string, is_?: boolean }[]> = ref([]);
 //显示和隐藏文字
-const xxxxxsssss = ()=>{
+const xxxxxsssss = () => {
     for (let theIdex = text.value.indexOf(word.value), lastIndex = 0; theIdex >= 0;) {
         // 替换文字
         for (let wordInex = 0; wordInex < word.value.length; wordInex++) {
             const shwoIndex = theIdex + wordInex;
             shwoArray.value[shwoIndex].is_ = true;
-            if (!shwoWord.value) {
-                if(inputWord.value[wordInex]){
-                    shwoArray.value[shwoIndex].t = inputWord.value[wordInex];
-                }else{
-                    shwoArray.value[shwoIndex].t = "";
-                }
+            if (inputWord.value[wordInex]) {
+                shwoArray.value[shwoIndex].t = inputWord.value[wordInex];
             } else {
-                shwoArray.value[shwoIndex].t = word.value[wordInex];
+                shwoArray.value[shwoIndex].t = undefined;
             }
         }
         lastIndex = theIdex;
@@ -28,10 +24,10 @@ const xxxxxsssss = ()=>{
     }
 }
 //加载文字和拼音
-const wwwzzzpppyyy = ()=>{
+const wwwzzzpppyyy = () => {
     shwoArray.value = [];
     for (const theChar of text.value) {
-        shwoArray.value.push({ t: theChar });
+        shwoArray.value.push({ ty: theChar, t: theChar });
     }
     const py = pinyin(word.value, { type: "array" });
     for (let theIdex = text.value.indexOf(word.value), lastIndex = 0; theIdex >= 0;) {
@@ -46,9 +42,9 @@ const wwwzzzpppyyy = ()=>{
     xxxxxsssss();
 }
 
-watch([text,word], () => {wwwzzzpppyyy()}, { immediate: true });
+watch([text, word], () => { wwwzzzpppyyy() }, { immediate: true });
 //党显示状态变化时
-watch([shwoWord,inputWord],()=>{xxxxxsssss()},{ immediate: false });
+watch([inputWord], () => { xxxxxsssss() }, { immediate: false });
 
 
 
@@ -57,34 +53,41 @@ watch([shwoWord,inputWord],()=>{xxxxxsssss()},{ immediate: false });
 </script>
 <template>
     <p class="pinyinP">
-    <div v-for="{ t, up , is_ } of shwoArray" :class="{aChar:true,mChar:is_}">
+    <div v-for="{ ty,t, up, is_ } of shwoArray" :class="{ aChar: true, mChar: is_ }">
         <div class="top">{{ up }}</div>
-        <div :class="{char:true,_char:is_,__char:!t}" >{{ t?t:"■" }}</div>
+        <div :class="{ char: true, _char: is_, __char: !t ,wordError:((!shwoWord)&&(t)&&(ty!=t)) }">{{ shwoWord?ty:(t ? t : "■")  }}</div>
     </div>
     </p>
 </template>
 <style scoped>
-.pinyinP{
+.wordError{
+    color: rgb(152, 0, 0);
+}
+.pinyinP {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     align-items: flex-end;
     justify-content: flex-start;
 }
+
 .aChar {
     display: inline-block;
 }
-.mChar{
+
+.mChar {
     margin-left: 0.3rem;
     margin-right: 0.3rem;
 }
-.top{
+
+.top {
     /* height: 4rem; */
     font-size: 3rem;
     line-height: 4rem;
     text-align: center;
 }
-.char{
+
+.char {
     font-size: 4rem;
     line-height: 4rem;
     /* min-height: 3rem;
@@ -92,14 +95,15 @@ watch([shwoWord,inputWord],()=>{xxxxxsssss()},{ immediate: false });
     text-align: center;
     padding-bottom: 0.2rem;
 }
-._char{
+
+._char {
     font-weight: bolder;
     /* font-size: 5em; */
     border-bottom: 0.2rem solid black;
     /* min-height: 8rem;
     min-width: 8rem; */
 }
-.__char{
+
+.__char {
     color: rgba(255, 255, 255, 0);
-}
-</style>
+}</style>
